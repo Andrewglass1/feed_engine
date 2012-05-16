2ab59a6dc0be2ea78a2875f06fbe3e92d7f8a1e3
Andrewglass1 <Andrewglass1@gmail.com>
2012-05-15 20:15:55 -0400

    reworked devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 require 'securerandom'
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   before_create :set_user_subdomain
   after_create :set_user_feed
   after_create :generate_api_key
--- db/migrate/20120508200206_devise_create_users.rb
+++ db/migrate/20120508200206_devise_create_users.rb
@@ -1,14 +1,14 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
+      #t.database_authenticatable :null => false
+      #t.recoverable
       t.string :display_name
       t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
-    add_index :users, :reset_password_token, :unique => true
+    #add_index :users, :email,                :unique => true
+    #add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
